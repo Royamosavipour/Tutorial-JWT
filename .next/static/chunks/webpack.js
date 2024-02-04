d9fd6951fd2561f151493abd29f599a4c559712a
royamousavi <roya2002_rm@yahoo.com>
2024-02-04 18:47:24 +0200

    ok

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "3cf5e9f5e0bd662c"; }
+/******/ 		__webpack_require__.h = function() { return "fda2594ae9e95e43"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
